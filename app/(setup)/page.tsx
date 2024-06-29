861cddee6d1eea2ee851e0ee988edc26c0235107
mellooDev <hvthuan.1810@gmail.com>
2024-06-30 01:01:24 +0700

    fix bug database, build create server UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,27 @@
+import { InitialModal } from "@/components/modals/initial-modal";
+import { db } from "@/lib/db";
+import { InitialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
 const SetupPage = async () => {
+    const profile = await InitialProfile()
+
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+
+                }
+            }
+        }
+    })
+
+    if(server) {
+        return redirect(`/servers/${server.id}`)
+    }
     return ( 
-        <div>Create a Server</div>
+        <InitialModal />
      );
 }
  
